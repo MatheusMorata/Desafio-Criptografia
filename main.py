928c74773ec018f98513c82d9d57ba5e02a361cb
MatheusMorata <matlucar09@hotmail.com>
2023-11-08 17:36:51 -0300

    feat MAIN

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ app = Flask(__name__)
 
 @app.route("/create", methods=['POST'])
 def Create():
-    teste = request.json
-    return teste
-
+    JSON = request.json
+    BD.Create(JSON['id'],JSON['userDocument'],JSON['creditCardToken'],JSON['value'])
+    return make_response(jsonify(BD.Read())) 
 @app.route("/read", methods=['GET'])
 def Read():
     return make_response(jsonify(BD.Read())) 
 
 @app.route("/update", methods=['PUT'])
 def Update():
-    return "" 
+    JSON = request.json
+    BD.Update(JSON["userDocument"], JSON["creditCardToken"], JSON["value"], JSON["id"])
+    return make_response(jsonify(BD.Read())) 
 
-@app.route("/delete", methods=['DELETE'])
-def Delete():
-    return ""
+@app.route("/delete/<int:id>", methods=['DELETE'])
+def Delete(id):
+    BD.Delete(id)
+    return make_response(jsonify(BD.Read())) 
+    
 
 app.run()
