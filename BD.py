8c159a4eb7b66da95843c9e1504530382f27fc10
MatheusMorata <matlucar09@hotmail.com>
2023-11-08 14:55:48 -0300

    feat BD

--- BD.py
+++ BD.py
@@ -4,7 +4,6 @@ import hashlib
 
 def Create(id, userDocument, creditCardToken, value):
     conexao = mysql.connect(host="localhost",user="root",password="",database="Desafio")
-
     sql = "INSERT INTO creditCard(id, userDocument, creditCardToken, value) VALUES (%s,%s,%s,%s)"
     cursor = conexao.cursor()
     
@@ -14,17 +13,31 @@ def Create(id, userDocument, creditCardToken, value):
     hash.update(userDocument.encode('utf-8'))
     hash1.update(creditCardToken.encode('utf-8'))
     userDocument = hash.hexdigest()
-    creditCardToken = hash1.hexdigest()
+    creditCardToken = hash1.hexdigest() 
 
     valores = (id,userDocument,creditCardToken,value)
-
     cursor.execute(sql,valores)
     conexao.commit()
     conexao.close()
 
 
-#def Update(id, userDocument, creditCardToken, value):
+def Update(id, userDocument, creditCardToken, value):
+    conexao = mysql.connect(host="localhost",user="root",password="",database="Desafio")
+    cursor = conexao.cursor()
+    sql = "UPDATE creditCard SET userDocument = %s, creditCardToken = %s, value = %s WHERE id = %s"
 
+    #Aqui são criadas as hashes
+    hash = hashlib.sha512()
+    hash1 = hashlib.sha512()
+    hash.update(userDocument.encode('utf-8'))
+    hash1.update(creditCardToken.encode('utf-8'))
+    userDocument = hash.hexdigest()
+    creditCardToken = hash1.hexdigest() 
+
+    valores = (userDocument,creditCardToken,value,id)
+    cursor.execute(sql,valores)
+    conexao.commit()
+    conexao.close()
 
 #def Read():
    
@@ -40,3 +53,4 @@ def Delete(id):
     conexao.close()
 
 
+Delete(10)
