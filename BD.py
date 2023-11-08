1963271ef26f49e0d30bc0cb1c772d221d960bcc
MatheusMorata <matlucar09@hotmail.com>
2023-11-08 10:37:22 -0300

    feat BD

--- BD.py
+++ BD.py
@@ -1,11 +1,23 @@
 import mysql.connector as mysql
+import hashlib
 
 conexao = mysql.connect(host="localhost",user="root",password="",database="Desafio")
 
 def Create(id, userDocument, creditCardToken, value):
-    cursor = conexao.cursor()
     sql = "INSERT INTO creditCard(id, userDocument, creditCardToken, value) VALUES (%s,%s,%s,%s)"
-    valores = (id, userDocument, creditCardToken, value)
+    cursor = conexao.cursor()
+    
+    #Aqui são criadas as hashes
+    hash = hashlib.sha512()
+    hash1 = hashlib.sha512()
+    hash.update(userDocument.encode('utf-8'))
+    hash1.update(creditCardToken.encode('utf-8'))
+    userDocument = hash.hexdigest()
+    creditCardToken = hash1.hexdigest()
+
+    valores = (id,userDocument,creditCardToken,value)
+
     cursor.execute(sql,valores)
     conexao.commit()
     conexao.close()
+
